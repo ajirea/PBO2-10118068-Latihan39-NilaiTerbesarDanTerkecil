b4cfb92a3c0dce74f6b19069e2d2499ae3fc1b43
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-14 13:47:36 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Program untuk menentukan nilai terbesar dan terkecil
+ * mahasiswa berbasis object, nilai berasal dari masukkan pengguna
+ */
+public class Petugas {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
